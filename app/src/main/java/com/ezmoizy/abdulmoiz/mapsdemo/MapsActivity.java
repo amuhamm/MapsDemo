eb364d77225351ce7fb00f1429a1190049c31d13
Abdul Moiz <ezmoizy11@gmail.com>
2016-02-12 21:59:43 -0500

    This is just a test commit & push.

--- app/src/main/java/com/ezmoizy/abdulmoiz/mapsdemo/MapsActivity.java
+++ app/src/main/java/com/ezmoizy/abdulmoiz/mapsdemo/MapsActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    // Just a test of GitHub.
 
     private GoogleMap mMap;
 
